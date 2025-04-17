ee25b287e68389578233aa105e0520e70c513e79
Satoshi Koda <77545144+ec22s@users.noreply.github.com>
2025-04-17 14:24:34 +0900

    fix for iOS and Android

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <cbl_flutter_ee/cbl_flutter_ee.h>
+#include <file_selector_windows/file_selector_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   CblFlutterEeRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("CblFlutterEe"));
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
 }
